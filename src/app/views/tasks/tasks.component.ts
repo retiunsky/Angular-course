1e0ce9e10d1350801d334f5b6b2c8bfcb46d4188
retiunsky <69350404+retiunsky@users.noreply.github.com>
2020-08-10 14:30:59 +0300

    добавили отображение всех задач (без фильтрации по выбранной категории)

---
+++ src/app/views/tasks/tasks.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Task} from '../../model/Task';
+import {DataHandlerService} from '../../service/data-handler.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[];
+
+  constructor(private datahandler: DataHandlerService) {
+  }
+
+  ngOnInit() {
+    this.tasks = this.datahandler.getTasks();
+  }
+
+}
